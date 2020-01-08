5ff8896b713a6efd8b36a99e43391654d7df2475
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-3.local>
2020-01-08 16:34:47 -0700

    final

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const port = 3005;
 
 const puppeteer = require("puppeteer");
 
+app.listen(port, async () => {
+  console.log("server running on", port);
 
-app.listen(port,async  () => {
-    console.log("server running on", port);
-    
+  console.log("hit");
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://auth.podium.com", { waitUntil: "networkidle2" });
+  await page.evaluate(() => {
+    document.getElementById("emailOrPhoneInput").click();
+  });
+  await page.keyboard.type(process.env.EMAIL);
+  await page.evaluate(() => {
+    document.getElementById("signInButton").click();
+  });
+  await page.waitFor(2000);
+  await page.evaluate(() => {
+    document.getElementById("passwordInput");
+  });
+  await page.keyboard.type(process.env.PASS);
+  await page.evaluate(() => {
+    document.getElementById("signInButton").click();
+  });
 
-        console.log('hit')
-        const browser=await puppeteer.launch()
-        const page=await browser.newPage()
-        await page.goto('https://podium.com')
-        await page.click(document.querySelector('div[class=mobile-ham]'))
-        await page.screenshot({path: './screenshots/shot.png'});
-        await browser.close()
-        console.log('browser shut down')
+  await page.waitForSelector("#selenium-insights");
 
+  await page.evaluate(() => {
+    document.getElementById("selenium-insights").click();
+  });
 
+  await page.evaluate(() => {
+    document.getElementById("selenium-reporting+").click();
+  });
+  await page.screenshot({ path: "./screenshots/shot1.png" });
+  await page.waitFor(10000);
+  await page.screenshot({ path: "./screenshots/shot.png" });
+  await browser.close();
+  console.log("browser shut down");
 });
