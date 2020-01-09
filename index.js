24b11b350cb6636463ec67df1d6ca99c5cc84ff9
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-3.local>
2020-01-09 15:05:28 -0700

    cant get the one thing

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.listen(port, async () => {
   console.log("server running on", port);
 
   console.log("hit");
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless:false, defaultViewport:null});
   const page = await browser.newPage();
   await page.goto("https://auth.podium.com", { waitUntil: "networkidle2" });
   await page.evaluate(() => {
@@ -29,37 +29,27 @@ app.listen(port, async () => {
   });
 
   await page.waitForSelector("#selenium-insights");
+    
+  await page.screenshot({ path: "./screenshots/shot1.png" });  
   
+  // await page.evaluate(() => {
+  //   document.getElementById("selenium-insights").click();
+  // });
   
-  await page.screenshot({ path: "./screenshots/shot1.png" });
-  // await page.waitFor('div[class=dm-content-toolbar-button]')
-  
-  
-  
-  
-  await page.evaluate(() => {
-    document.getElementById("selenium-insights").click();
-  });
-  
-  await page.evaluate(() => {
-    document.getElementById("selenium-reporting+").click();
-  });
-  await page.waitFor(10000);
+  // await page.evaluate(() => {
+  //   document.getElementById("selenium-reporting+").click();
+  // });
+
+  await page.goto('https://insights.podium.com/reporting-plus?location_id=54673', {waitUntil:'networkidle2'})
   // text: "NPS Export (Based on Service Date)"
-  await page.evaluate(()=>{
-    let nodeList=document.querySelectorAll('a.kpi_title')
-    let thingToClick
-    for (let i=0;i<nodeList.length;i++){
-      if(nodeList[i].text.contains('NPS Export')){
-      thingToClick=nodeList[i] 
-      }
-    }
-    document.querySelector(thingToClick)
-  })
-
-
-
+  await page.waitFor(10000)
   await page.screenshot({ path: "./screenshots/shot.png" });
-  await browser.close();
+await page.evaluate(()=>{
+
+console.log(document.querySelector('#domo-reporting-content'))
+  
+})
+
+  // await browser.close();
   console.log("browser shut down");
 });
