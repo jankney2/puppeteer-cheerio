5918972f4e5e533f2ed428853aa3d510f27c4871
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-3.local>
2020-01-09 13:29:46 -0700

    final

--- index.js
+++ index.js
@@ -29,16 +29,36 @@ app.listen(port, async () => {
   });
 
   await page.waitForSelector("#selenium-insights");
-
+  
+  
+  await page.screenshot({ path: "./screenshots/shot1.png" });
+  // await page.waitFor('div[class=dm-content-toolbar-button]')
+  
+  
+  
+  
   await page.evaluate(() => {
     document.getElementById("selenium-insights").click();
   });
-
+  
   await page.evaluate(() => {
     document.getElementById("selenium-reporting+").click();
   });
-  await page.screenshot({ path: "./screenshots/shot1.png" });
   await page.waitFor(10000);
+  // text: "NPS Export (Based on Service Date)"
+  await page.evaluate(()=>{
+    let nodeList=document.querySelectorAll('a.kpi_title')
+    let thingToClick
+    for (let i=0;i<nodeList.length;i++){
+      if(nodeList[i].text.contains('NPS Export')){
+      thingToClick=nodeList[i] 
+      }
+    }
+    document.querySelector(thingToClick)
+  })
+
+
+
   await page.screenshot({ path: "./screenshots/shot.png" });
   await browser.close();
   console.log("browser shut down");
