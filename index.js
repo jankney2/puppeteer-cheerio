ab6e6ce7a42f9bf3ed5d5a6107abd44082e1c22b
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-3.local>
2020-01-21 11:34:42 -0700

    final before server

--- index.js
+++ index.js
@@ -33,45 +33,25 @@ app.listen(port, async () => {
 
   await page.waitForSelector("#selenium-insights");
 
-  await page.screenshot({ path: "./screenshots/shot1.png" });
-
-  // await page.evaluate(() => {
-  //   document.getElementById("selenium-insights").click();
-  // });
-
-  // await page.evaluate(() => {
-  //   document.getElementById("selenium-reporting+").click();
-  // });
-
   await page.goto(
     "https://insights.podium.com/reporting-plus?location_id=54673",
     { waitUntil: "networkidle2" }
   );
-  // text: "NPS Export (Based on Service Date)"
   await page.waitFor(10000);
-  await page.screenshot({ path: "./screenshots/shot.png" });
-  // let frames=await page.frames()
-  // let correctFrame=frames.find(frame=>{
-  //   frame.id==='#domo-reporting-content'
-  // })
-
-  // console.log(correctFrame, 'correct!')
 
   await page.goto(
     "https://podium-ss.domo.com/page/ss:149:797560795/kpis/details/678494653",
     { waitUntil: "networkidle2" }
   );
 
-  await page.waitForSelector('i.icon-wrench')
-  await page.evaluate( async () => {
-    await document.querySelector('i.icon-wrench').click()
+  await page.waitForSelector("i.icon-wrench");
+  await page.evaluate(async () => {
+    await document.querySelector("i.icon-wrench").click();
 
-    await document.querySelectorAll('div.db-text-body.label')[1].click()
-    await document.getElementById('export-csv').click()
-
+    await document.querySelectorAll("div.db-text-body.label")[1].click();
+    await document.getElementById("export-csv").click();
   });
-  await page.waitFor(3000)
-  await page.goto('chrome://downloads', {waitUntil:'networkidle2'})
-  // await browser.close();
+  await page.waitFor(3000);
+  await browser.close();
   console.log("browser shut down");
 });
